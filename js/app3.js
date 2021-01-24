a1950d73302fb959cbf0424bdaf1c1042a277719
Ahmad AlSabbagh <a.sameer1999@gmail.com>
2021-01-25 01:26:51 +0200

    presentation edition

--- js/app3.js
+++ js/app3.js
@@ -1,6 +1,6 @@
 var renderDiv = document.getElementById("renderTop");
 
-function topR(array) {
+function topR(array, idx) {
     var top = 0;
     var topR = [];
     for (var i = 0; i < array.length; i++) {
@@ -13,28 +13,36 @@ function topR(array) {
     var index = array.indexOf(topR);
     array.splice(index, 1);
     var rateRender = document.getElementById("renderTop");
+
+    var container = document.createElement('div');
+    console.log(index);
+    if (idx === 0) {
+        container.className = 'top-container';
+    }
     var name = document.createElement('h1');
     var rimg = document.createElement('img');
     rimg.src = "/img/" + topR.img;
 
     name.textContent = topR.name;
-    rateRender.appendChild(name);
+    container.appendChild(name);
     var br = document.createElement('br');
     for (var j = 0; j < top; j++) {
         console.log("in");
         var img = document.createElement('img');
         img.src = '/img/star.png';
         img.style.width = '50px';
-        rateRender.appendChild(img);
+        container.appendChild(img);
     }
-    rateRender.appendChild(br);
-    rateRender.appendChild(rimg);
+    container.appendChild(br);
+    container.appendChild(rimg);
+
+    rateRender.appendChild(container);
 }
 
 // 
 if (localStorage.getItem("rate") != null) {
     arrayofRest2 = JSON.parse(localStorage.getItem('rate'));
     for (let index = 0; index < 10; index++) {
-        topR(arrayofRest2);
+        topR(arrayofRest2, index);
     }
 }
